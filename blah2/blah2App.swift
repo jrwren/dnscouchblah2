569737d1084771673ec1031594b20e714fea2430
Jay Wren <jrwren@xmpt.net>
2023-10-30 18:37:28 -0400

    Initial Commit

---
+++ blah2/blah2App.swift
@@ -0,0 +1,17 @@
+//
+//  blah2App.swift
+//  blah2
+//
+//  Created by Jay Wren on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct blah2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
